4dc28fb415882ad8e55ce31b82c04c5275a79575
Axel <souza93@hotmail.fr>
2017-04-26 16:24:24 +0200

    Cleanup docker files: untagged containers and images

---
+++ docker-cleanup.sh
@@ -0,0 +1,6 @@
+# Install for cleanup docker files: untagged containers and images
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
