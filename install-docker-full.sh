0bb38f52389ecb205d37bb17bea71602e489811d
Axel <souza93@hotmail.fr>
2017-04-26 16:18:23 +0200

    delete docker-cleanup

--- install-docker-full.sh
+++ install-docker-full.sh
@@ -10,10 +10,3 @@ COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tag
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-
-# Install docker-cleanup 
-cd /tmp
-git clone https://gist.github.com/76b450a0c986e576e98b.git
-cd 76b450a0c986e576e98b
-sudo mv docker-cleanup /usr/local/bin/docker-cleanup
-sudo chmod +x /usr/local/bin/docker-cleanup
